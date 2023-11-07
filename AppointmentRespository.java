98c2253b78a7201ba3ee2db568c1f5ef57639ae5
Harshini-Menta <mentaharshini@gmail.com>
2023-11-07 21:01:35 +0530

    Added Successfully

--- AppointmentRespository.java
+++ AppointmentRespository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.home.entity.Appointment;
 
 public interface AppointmentRespository extends CrudRepository<Appointment, Integer> {
+    //surya
 
 }
